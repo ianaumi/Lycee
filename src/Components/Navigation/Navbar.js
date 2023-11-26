0b4f5572caff37d78a9544ccb5f684e45d0dfaab
umi <lumanog.dev@gmail.com>
2023-11-26 15:35:58 +0800

    Updated to extend component

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -1,5 +1,25 @@
-export default function(){
-    return(
-        <h2>Navbar</h2>
-    )
+import logo from '../../Assets/lycee-logo.png'
+import { Component } from 'react'
+
+export default class Navbar extends Component(){
+    render(){
+        return(
+            <>
+                <nav>
+                    <a href='/'>
+                        <img src={logo}></img>
+                    </a>
+    
+                    <div>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/'>About</a></li>
+                            <li><a href='/'>Map</a></li>
+                            <li><a href='/'>Events</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </>
+        )
+    }
 }
