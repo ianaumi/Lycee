36de6e1cf2f0264be320244258befc14aa15e49d
umi <lumanog.dev@gmail.com>
2023-11-27 00:06:39 +0800

    Added: map container with buildings

--- src/Components/Map-page/Map.js
+++ src/Components/Map-page/Map.js
@@ -1,9 +1,20 @@
 import './MapStyles.css';
+import buildings from '../../Assets/buildings.svg';
 
 export default function Map() {
     return(
         <>
-        <h1>Campus Map</h1>
+        <div className='map-container'>
+            <h1>Campus Map</h1>
+            <img src={buildings} alt='buildings' className='campus-map'/>
+
+            <a href="/">
+                <div className="location location-coecsa"></div>
+            </a>
+            <a href="/">
+                <div className="location location-annex"></div>
+            </a>
+        </div>
         </>
     )
 }
