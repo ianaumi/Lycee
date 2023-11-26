5ad92dd3dc55a42446754527152d0af752d8743b
umi <lumanog.dev@gmail.com>
2023-11-26 15:36:05 +0800

    Added: navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './index.css';
+import Navbar from './Components/Navigation/Navbar'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <Navbar/>
     </div>
   );
 }
