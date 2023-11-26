19c19b6e5e1ca3f4dcdc90b994a558a180dc5cb8
umi <lumanog.dev@gmail.com>
2023-11-26 23:06:07 +0800

    Added: Map component

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,9 +1,11 @@
 import Navbar from "../Components/Navigation/Navbar";
+import Map from "../Components/Map-page/Map";
 
 export default function Home(){
     return(
         <>
         <Navbar/>
+        <Map/>
         </>
     )
 }
