96bf68d9201ece1a1e53b29340dbb4423ebe168f
umi <lumanog.dev@gmail.com>
2023-11-27 20:42:15 +0800

    Added:  Map page

---
+++ src/Routes/Map.js
@@ -0,0 +1,10 @@
+import Navbar from "../Components/Navigation/Navbar"
+
+export default function Map () {
+    return(
+        <>
+        <Navbar/>
+        <h2>Map Page</h2>
+        </>
+    )
+}
