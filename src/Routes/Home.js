f1d3690707b0e7e62f0adfd52a138e9a98944e6f
umi <lumanog.dev@gmail.com>
2023-11-27 22:06:18 +0800

    Added: card page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,11 +1,13 @@
 import Navbar from "../Components/Navigation/Navbar";
 import Map from "../Components/Map-page/Map";
+import Card from "../Components/Card/Card"
 
 export default function Home(){
     return(
         <>
         <Navbar/>
         <Map/>
+        <Card/>
         </>
     )
 }
