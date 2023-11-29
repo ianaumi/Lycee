91a4deb051a4402b77b3607d99503c5646e84981
umi <lumanog.dev@gmail.com>
2023-11-29 04:26:14 +0000

    Added: Event component

--- src/Routes/Home-page.js
+++ src/Routes/Home-page.js
@@ -1,13 +1,14 @@
 import Navbar from "../Components/Navigation-component/Navbar";
 import Map from "../Components/Map-component/Map";
 import Card from "../Components/Card-component/Card"
+import Event from "../Components/Events-component/Event";
 
 export default function Home(){
     return(
         <>
         <Navbar/>
         <Map/>
-        <Card/>
+        <Event/>
         </>
     )
 }
