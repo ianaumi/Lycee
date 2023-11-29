a8b16aa0448531ca6357a2aeb6af3835eb220c93
umi <lumanog.dev@gmail.com>
2023-11-29 04:44:06 +0000

    Updated: card component added cards

--- src/Components/Events-component/Event.js
+++ src/Components/Events-component/Event.js
@@ -1,10 +1,17 @@
 import ".//EventStyles.css";
+import Card from "../Card-component/Card";
 
 export default function Event(){
     return(
         <>
-        <div className="Event-container">
+        <div className="event-container">
             <h1>Upcomming Events</h1>
+
+            <div className="event-cards">
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
         </div>
         </>
     )
