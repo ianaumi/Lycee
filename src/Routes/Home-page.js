ef49c3f28050f96487621ad69621d55ede58a6db
umi <lumanog.dev@gmail.com>
2023-11-29 08:22:59 +0000

    Removed card componnent

--- src/Routes/Home-page.js
+++ src/Routes/Home-page.js
@@ -1,6 +1,5 @@
 import Navbar from "../Components/Navigation-component/Navbar";
 import Map from "../Components/Map-component/Map";
-import Card from "../Components/Card-component/Card"
 import Event from "../Components/Events-component/Event";
 
 export default function Home(){
