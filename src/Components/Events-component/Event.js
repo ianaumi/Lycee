67f5ad1a304709414bb9266d216e70327d2756a0
umi <lumanog.dev@gmail.com>
2023-11-29 04:28:10 +0000

    Added: title

---
+++ src/Components/Events-component/Event.js
@@ -0,0 +1,11 @@
+import ".//EventStyles.css";
+
+export default function Event(){
+    return(
+        <>
+        <div className="Event-container">
+            <h1>Upcomming Events</h1>
+        </div>
+        </>
+    )
+}
