1ac944db54599cd7102c5616fe02d745da86777a
umi <lumanog.dev@gmail.com>
2023-11-30 19:34:10 +0800

    Removed: Map Component

--- src/Routes/Home-page.js
+++ src/Routes/Home-page.js
@@ -1,12 +1,10 @@
 import Navbar from "../Components/Navigation-component/Navbar";
-import Map from "../Components/Map-component/Map";
 import Event from "../Components/Events-component/Event";
 
 export default function Home(){
     return(
         <>
         <Navbar/>
-        <Map/>
         <Event/>
         </>
     )
