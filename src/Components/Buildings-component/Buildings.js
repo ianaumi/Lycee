9d18daec909993a7dc3f43b3a14499d84929c14c
umi <lumanog.dev@gmail.com>
2023-11-30 20:29:59 +0800

    Added building component

---
+++ src/Components/Buildings-component/Buildings.js
@@ -0,0 +1,13 @@
+import "./Buildings.css";
+import BuildingCard from "./BuildingCard";
+export default function Buildings(){
+
+    return(
+        <>
+        <div className="Buildings-Container">
+            <BuildingCard/>
+        </div>
+
+        </>
+    )
+}
--- src/Routes/Home-page.js
+++ src/Routes/Home-page.js
@@ -1,10 +1,12 @@
 import Navbar from "../Components/Navigation-component/Navbar";
 import Event from "../Components/Events-component/Event";
+import Buildings from "../Components/Buildings-component/Buildings";
 
 export default function Home(){
     return(
         <>
         <Navbar/>
+        <Buildings/>
         <Event/>
         </>
     )
