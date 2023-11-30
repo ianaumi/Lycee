e3e2105a216f66a906c83003911d710a3e5871ce
umi <lumanog.dev@gmail.com>
2023-11-30 22:12:35 +0800

    Added card component

--- src/Components/Buildings-component/Buildings.js
+++ src/Components/Buildings-component/Buildings.js
@@ -1,10 +1,19 @@
 import "./Buildings.css";
 import BuildingCard from "./BuildingCard";
+import img1 from "../../Assets/coecsa-art.png" 
+
 export default function Buildings(){
 
     return(
         <>
-        <div className="Buildings-Container">
+        <h1>Buildings</h1>
+        <div className="buildings-container">
+            <BuildingCard
+            title="COECSA"
+            sub="College of Engineering, Computer Studies and Architecture"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget eros non ipsum maximus mollis ut vel neque. Ut non ultricies sapien. Nulla facilisi."
+            img={img1}
+            />
             <BuildingCard/>
         </div>
 
