e82e8ad6df4c96b46a274863ea31748531a60bac
umi <lumanog.dev@gmail.com>
2023-12-06 06:27:11 +0800

    Update building-card

--- src/Components/Campus-component/BuildingCard.js
+++ src/Components/Campus-component/BuildingCard.js
@@ -1,5 +1,6 @@
 import './BuildingStyles.css';
 import coecsa from '../../Assets/coecsa-art.png';
+import { Link } from 'react-router-dom';
 import { Button } from '../Button-component/Button';
 
 
@@ -9,10 +10,12 @@ export default function BuildingCard(){
         <div className="building-card">
             <img src={coecsa}/>
             <div className="building-content">
-            <h3>COECSA</h3>
-            <h4>North</h4>
-            <p>Lorem ipsum dolonsectetur adipiscing elitor sit amet, consectetur adipiscing elit. Cras eu eleifend sapien. </p>
-            <button>Navigate</button>
+                <h3>COECSA</h3>
+                <h4>North</h4>
+                <p>Lorem ipsum dolonsectetur adipiscing elitor sit amet, consectetur adipiscing elit. Cras eu eleifend sapien. </p>
+                <Link to='/Campus'>
+                    <button type='button'>Navigate</button>
+                </Link>
             </div>
         </div>
         </>
