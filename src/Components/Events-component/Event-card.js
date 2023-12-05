4d30482c3dd23bf176220cc3b93afb545ff041a5
umi <lumanog.dev@gmail.com>
2023-12-05 04:53:41 +0000

    Created card template

--- src/Components/Events-component/Event-card.js
+++ src/Components/Events-component/Event-card.js
@@ -1,16 +1,22 @@
-import './Event-CardStyles.css';
+import './EventCardStyles.css';
 
 export default function() {
     return(
         <>
             <div className='event-container'>
-                <img src='/'/>
-                <h3>Event title</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis eros. In a ante quis quam viverra finibus. Curabitur vitae vestibulum lacus. Aenean hendrerit tortor at neque iaculis, eu euismod lacus malesuada.</p>
-                <div className='department-info'>
-                    <img src='/'/>
-                    <h4>Department</h4>
-                    <time>MM DD YYYY</time>
+                <div className='event-img'>
+                    <img src='https://img.freepik.com/premium-vector/photo-icon-picture-icon-image-sign-symbol-vector-illustration_64749-4409.jpg'/>
+                </div>
+                <div className='event-content'>
+                    <h3>Event title</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis eros. In a ante quis quam viverra finibus. Curabitur vitae vestibulum lacus. Aenean hendrerit tortor at neque iaculis, eu euismod lacus malesuada.</p>
+                    <div className='event-info'>
+                        <div className='logo'>
+                            <img src='https://i.pinimg.com/originals/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg'/>
+                        </div>
+                        <h4>Department</h4>
+                        <time>MM DD YYYY</time>
+                    </div>
                 </div>
             </div>
         </>
--- src/Routes/Home-page.js
+++ src/Routes/Home-page.js
@@ -1,10 +1,12 @@
 import Navbar from "../Components/Navigation-component/Navbar";
-
+import Campus from "../Components/Campus-component/Campus";
+import EventCard from "../Components/Events-component/Event-card";
 
 export default function Home(){
     return(
         <>
         <Navbar/>
+        <EventCard/>
         </>
     )
 }
