48db5362f77e999fdb6ffcf07cdf5392eb588df9
umi <lumanog.dev@gmail.com>
2023-12-05 05:18:37 +0000

    Updated card component

--- src/Components/Events-component/Event-card.js
+++ src/Components/Events-component/Event-card.js
@@ -9,7 +9,6 @@ export default function() {
                 </div>
                 <div className='event-content'>
                     <h3>Event title</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis eros. In a ante quis quam viverra finibus. Curabitur vitae vestibulum lacus. Aenean hendrerit tortor at neque iaculis, eu euismod lacus malesuada.</p>
                     <div className='event-info'>
                         <div className='logo'>
                             <img src='https://i.pinimg.com/originals/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg'/>
@@ -17,6 +16,7 @@ export default function() {
                         <h4>Department</h4>
                         <time>MM DD YYYY</time>
                     </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis eros. In a ante quis quam viverra finibus. Curabitur vitae vestibulum lacus. Aenean hendrerit tortor at neque iaculis, eu euismod lacus malesuada.</p>
                 </div>
             </div>
         </>
