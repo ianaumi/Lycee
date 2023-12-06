9f22227e3c658ad676f8293289c087c1599412bd
umi <lumanog.dev@gmail.com>
2023-12-07 02:18:00 +0800

    Added developer component

--- src/Components/About-component/AboutLycee.js
+++ src/Components/About-component/AboutLycee.js
@@ -8,12 +8,12 @@ export default function AboutLycee () {
                 <div className="about-lycee-container">
                     <div className="content">
                         <div className="about-lycee-left">
-                            <h1>About lycee</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et orci quis quam semper placerat. Etiam scelerisque, ante id semper suscipit, nulla enim tristique est, in maximus ex dolor nec nisi. Nunc non auctor orci, ac rhoncus nisi.</p>
+                        <h1>About lycee</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et orci quis quam semper placerat. Etiam scelerisque, ante id semper suscipit, nulla enim tristique est, in maximus ex dolor nec nisi. Nunc non auctor orci, ac rhoncus nisi.</p>
                         </div>
-                        <div className="about-lycee-right">
-                            <img src={lyceeLogo} alt="" />
-                        </div>
+                    <div className="about-lycee-right">
+                        <img src={lyceeLogo} alt="" />
+                    </div>
                     </div>
                 </div>
             </div>
--- src/Routes/About-page.js
+++ src/Routes/About-page.js
@@ -1,12 +1,14 @@
 import Navbar from "../Components/Navigation-component/Navbar";
 import AboutLycee from "../Components/About-component/AboutLycee";
 import { AiFillAliwangwang } from "react-icons/ai";
+import Developer from "../Components/About-component/Developer";
 
 export default function AboutPage(){
     return(
         <>
         <Navbar/>
         <AboutLycee/>
+        <Developer/>
         </>
     )
 }
