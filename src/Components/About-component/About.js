3e3cf03d44fcb089cdd90b31c56c63539b4d693a
umi <lumanog.dev@gmail.com>
2023-12-06 19:57:43 +0800

    Added about component

---
+++ src/Components/About-component/About.js
@@ -0,0 +1,11 @@
+import './AboutStyles.css'
+
+export default function About () {
+    return(
+        <>
+        <div className="about-wrapper">
+            <h2>About</h2>
+        </div>
+        </>
+    )
+}
