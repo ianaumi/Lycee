baa884b10f678ff1dc967c705455547939606818
umi <lumanog.dev@gmail.com>
2023-12-06 08:10:00 +0000

    Created campus page

---
+++ src/Routes/CampusPage.js
@@ -0,0 +1,10 @@
+import Navbar from "../Components/Navigation-component/Navbar"
+
+export default function CampusPage () {
+    return(
+        <>
+        <Navbar/>
+            <h2>Campus Page</h2>
+        </>
+    )
+}
