83262237bf27a72fae63457a87a6156ea52e1f8f
umi <lumanog.dev@gmail.com>
2023-12-06 20:12:25 +0800

    Created about lycee template

---
+++ src/Components/About-component/AboutLycee.js
@@ -0,0 +1,19 @@
+import './AboutStyles.css'
+
+export default function AboutLycee () {
+    return(
+        <>
+            <div className="about-lycee-wrap">
+                <div className="about-lycee-container">
+                    <div className="about-lycee-left">
+                        <h1>About lycee</h1>
+                        <p>description here</p>
+                    </div>
+                    <div className="about-lycee-right">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
