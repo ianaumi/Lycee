1e5a78e6b9ac9584e100a447b149844c1d5a58b4
umi <lumanog.dev@gmail.com>
2023-12-06 19:57:27 +0800

    Added about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Routes/Home-page';
 import CampusPage from './Routes/CampusPage';
+import AboutPage from './Routes/About-page';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
           <Route path="/"element={<Home/>}/>
+          <Route path="/About"element={<AboutPage/>}/>
           <Route path="/Campus"element={<CampusPage/>}/>
       </Routes>
     </div>
