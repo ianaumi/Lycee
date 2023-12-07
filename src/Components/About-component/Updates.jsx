23755603722ebb3ee678db9f2629b6e04008533f
umi <lumanog.dev@gmail.com>
2023-12-07 21:49:51 +0800

    Added update files

---
+++ src/Components/About-component/Updates.jsx
@@ -0,0 +1,15 @@
+import './UpdateStyles.css';
+
+export default function () {
+
+    return(
+        <>
+            <div className="update-wrap">
+                <div className="update-container">
+                    <h1></h1>
+                </div>
+            </div>
+        </>
+    )
+    
+}
