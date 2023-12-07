b257c09107edb759f81fa61d00c841695cd05421
umi <lumanog.dev@gmail.com>
2023-12-08 00:13:03 +0800

    coecsa building test

--- src/Routes/CampusPage.js
+++ src/Routes/CampusPage.js
@@ -1,10 +1,13 @@
-import Navbar from "../Components/Navigation-component/Navbar"
+import Navbar from "../Components/Navigation-component/Navbar";
+import Campus from "../Components/Campus-component/Campus";
+import Coecsa from "../Components/Campus-component/Buildings/coecsa-building/Coecsa";
 
 export default function CampusPage () {
     return(
         <>
         <Navbar/>
-            <h2>Campus</h2>
+        <Campus/>
+        <Coecsa/>
         </>
     )
 }
