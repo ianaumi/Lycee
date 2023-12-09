5fe226edc89fbb4b2e2a2a5b93ceba3792a8350d
umi <lumanog.dev@gmail.com>
2023-12-09 22:17:22 +0800

    Updated building data

--- src/Components/Campus-component/BuildingData.js
+++ src/Components/Campus-component/BuildingData.js
@@ -1,26 +1,50 @@
 
+
+import coecsaBuilding from "../../Assets/coecsa-building.png";
+import annexBuilding from "../../Assets/annex-building.png";
+import libraryBuilding from "../../Assets/library-building.png";
+import soteroBuilding from "../../Assets/sotero-building.png";
+import jplBuilding from "../../Assets/jpl-building.png";
+
 export const BuildingData = [
     {
-        buildingImg: "/",
+        buildingImg: coecsaBuilding,
         buildingTitle : "COECSA",
-        buildingLocation : "NORTH",
-        buildingDesc: "p",
+        buildingLocation : "NORTH", 
+        buildingDesc: "Securty | Le cafe | Auditorium | ICT Department | Nutrition Clinic | Audio Visual Theater | Beauty Laboratory | Leaf Hotel...",
         buildingName : "coecsa",
     },
 
     {
-        buildingImg: "/",
+        buildingImg: annexBuilding,
         buildingTitle : "ANNEX",
         buildingLocation : "WEST",
-        buildingDesc: "A",
+        buildingDesc: "Purchasing | Student Center | Innovation and Incubation Center | Campus Ministry | Bookstore | Legal Aid...",
         buildingName : "annex",
+    },
+{
+        buildingImg:  libraryBuilding,
+        buildingTitle : "ARC",
+        buildingLocation : "NORTH",
+        buildingDesc: "Periodicals / Thesis Section | E-Library | Coffee Shop | Circular Section | Highschool Section...",
+        buildingName : "sotero",
+    },
+    
+    {
+        buildingImg:  soteroBuilding,
+        buildingTitle : "SOTERO",
+        buildingLocation : "WEST",
+        buildingDesc: "Securty | Le cafe | Auditorium | ICT Department | Nutrition Clinic | Audio Visual Theater | Beauty Laboratory | Leaf Hotel...",
+        buildingName : "sotero",
     },
 
     {
-        buildingImg: "/",
-        buildingTitle : "SOTERO",
+        buildingImg:  jplBuilding,
+        buildingTitle : "JPL",
         buildingLocation : "EAST",
-        buildingDesc: "AA",
+        buildingDesc: "Communications And Public Affairs Department | Registrar's Office | Accounting Department | Accerdiation Center...",
         buildingName : "sotero",
     },
+
+    
 ]
